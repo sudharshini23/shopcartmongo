390e4ca5456452f549e1278639ed23a6ace8e77b
sudharshini23 <sudarsh25@gmail.com>
2023-09-08 15:53:09 +0530

    Cart Items Addition

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('64facad5624dd84deb82cc19')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
